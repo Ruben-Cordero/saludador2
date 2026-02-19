b32ff3c64b5fb12eda8d960f3ca16769882ae650
Rubén Cordero <garu1812@gmail.com>
2026-02-18 21:58:12 -0400

    feat: saludo automatico segun la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,16 @@
-function saludar (nombre) {
-  return  "Hola " + nombre;
-}
+function saludar(nombre) {
+  const fecha = new Date();
+  const hora = fecha.getHours();
+  let saludo = "Hola";
 
+  if (hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora < 19) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  return saludo + " " + nombre;
+}
 export default saludar;
